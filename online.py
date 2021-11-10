c818b92acf451dffafe567772205352b5219e143
William W <81191122+websterwh16@users.noreply.github.com>
2021-11-10 05:39:34 -0800

    Add files via upload

---
+++ online.py
@@ -0,0 +1,58 @@
+
+from requests import get
+import threading
+from discord.ext import commands
+import discord
+import pyautogui
+import time
+import re
+import http.client
+import random
+import json
+import requests
+from threading import Thread
+from requests import Session
+import base64
+import string
+import sys
+import os
+import websocket
+from os import system
+import discum
+
+
+
+ur = 'https://discord.com/api/v9/channels/messages'
+title = 'PussyKiller'
+system(f'title {title}')
+tokens = open('tokens.txt', 'r').read().splitlines()
+
+def activity(token, act):
+    while True:
+        ws = websocket.WebSocket()
+        actt = 'Online'
+        ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+        gjs = {'name': act,
+                    'type': 0}
+        auth = {'op': 2,
+                'd': {'token': token,
+                    'properties': {'$os': sys.platform,
+                                    '$browser': 'RTB',
+                                    '$device': f"{sys.platform} Device"},
+                    'presence': {'game': gjs,
+                                'status': actt,
+                                'since': 0,
+                                'afk': False}},
+                's': None,
+                't': None}
+             
+        ws.send(json.dumps(auth))
+        time.sleep(2)
+        print('[+] Activity Status: ')
+
+
+#text = input('Activity Status: ')
+
+for token in tokens:
+    threading.Thread(target=activity, args=(token, 'a')).start()
+
